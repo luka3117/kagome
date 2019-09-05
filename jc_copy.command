aabf34062186f2a819794f08549619188568d346
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2019-09-06 02:03:58 +0900

    data cleaning

---
+++ jc_copy.command
@@ -0,0 +1,3 @@
+cd `dirname $0`
+cp /Users/jclee/Dropbox/\[様式\]/jc.rmd aa.rmd
+cp /Users/jclee/Dropbox/\[様式\]/jc.csv aa.csv
--- カゴメ購買(解析).R
+++ カゴメ購買(解析).R
@@ -1,18 +1,35 @@
 # see : https://github.com/opetchey/RREEBES/wiki/Reading-data-and-code-from-an-online-github-repository
 # see : https://stackoverflow.com/questions/14441729/read-a-csv-from-github-into-r
+setwd("/Users/jclee/Desktop/kagome")
 
-library(RCurl)
+suppressMessages(library(RCurl))
 suppressMessages(library(tidyverse))
 # install.packages("RCurl")
 
-koubai.address <- getURL(
-  "https://raw.githubusercontent.com/luka3117/kagome/master/total.csv"
-)
-koubai<-read.csv(text = koubai.address, row.names = 1) %>% tbl_df()
+# read from web
+# koubai.address <- getURL(
+#   "https://raw.githubusercontent.com/luka3117/kagome/master/total.csv"
+# )
+# koubai<-read.csv(text = koubai.address, row.names = 1) %>%
+#   tbl_df() %>% select(-平均価格, -ブランド詳細)
+
+koubai <- read.csv("total.csv", row.names = 1) %>% tbl_df
+
+# factor 型データ
+koubai.factor <-
+  koubai %>% select(age, sex, データ種別, カテゴリー大, メーカー)
+
+# 数値 型データ
+koubai.numeric<-
+koubai %>% select_if(is.numeric) %>%
+# select(-JANコード,-累積シェア, -金額シェア, -枝番, -購入率) %>% 
+select(順位,X100人あたり購入金額,購入者あたり購入金額) 
+
+# 購入先データ
+koubai.store<-koubai %>% select(スーパー:その他購入先) %>% round(2)
 
 
-koubai %>% dim()
-koubai %>% colnames()
-koubai %>% summary()
 
+# koubai %>% select_if(is.factor) %>% 
+#     lapply(levels) %>% sapply(.,length) %>% sort() 
 
